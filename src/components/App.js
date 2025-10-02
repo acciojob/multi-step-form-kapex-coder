ed18a6321c03eed7fe205b7a358c133ad2d450d5
kapex-coder <kapex.coder@gmail.com>
2025-10-02 13:57:35 +0530

    Submission 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,47 @@
-
-import React,{useState} from "react";
-import Step from './Step.js'
+import React, { useState } from "react";
+import Step from "./Step.js";
 
 const App = () => {
-  const [index,setIndex]=useState(1);
-  const [formData,setFormData]=useState({
-    firstName:"",
-     lastName:"",
-    make:"",
-    model:"",
-    card_info:"",
-    expiry_date:""
+  const [index, setIndex] = useState(1);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    make: "",
+    model: "",
+    card_info: "",
+    expiry_date: "",
+  });
 
-});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-const handleChange=(e)=>{
-  const {name,value}=e.target;
-  setFormData({...formData,[name]:value})
-}
+  const handlePrev = (index) => {
+    setCurrentStep(index - 1);
+  };
+  const handleNext = (index) => {
+    setCurrentStep(index + 1);
+  };
 
-  const handlePrev=(index)=>{
-    setIndex(index-1);
-  }
-    const handleNext=(index)=>{
-    setIndex(index+1);
-  }
-
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault(); // Prevents page reload
-console.log("Form Data Submitted:", formData);
-  }
+    console.log("Form Data Submitted:", formData);
+  };
 
   return (
     <div>
-      <Step formData={formData} index={index}
-      handleChange={handleChange}
-      handlePrev={handlePrev}
-      handleNext={handleNext}
-      handleSubmit={handleSubmit}/>
+      <Step
+        formData={formData}
+        currentStep={currentStep}
+        handleChange={handleChange}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+        handleSubmit={handleSubmit}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Step.js
+++ src/components/Step.js
@@ -1,54 +1,105 @@
-import React from 'react'
+import React from "react";
 
-const Step = ({index,formData,handleChange,handlePrev,handleNext,handleSubmit}) => {
+const Step = ({
+  currentStep,
+  formData,
+  handleChange,
+  handlePrev,
+  handleNext,
+  handleSubmit,
+}) => {
   return (
-    <div>
-        <h1>Customer Details</h1>
-        <br></br>
-  {index==1 &&
-   (<div id="step1">
-   <label htmlFor="first_name">First Name:</label>
-      <input type="text"  id="first_name" name="firstName" value={formData.firstName} onChange={handleChange}/>
-      <br></br><br></br>
-        <label htmlFor="last_name">Last Name:</label>
-      <input type="text"  id="last_name" name="lastName" value={formData.lastName} onChange={handleChange}/>
-      <br></br><br></br>
-      </div>)
-  }
-  {index==2 &&
-  ( <div id="step2">
-        <label htmlFor="car_price">Make:</label>
-      <input type="text"  id="car_price" name="make" value={formData.make} onChange={handleChange}/>
-      <br></br><br></br>
-      <label htmlFor="model">Model:</label>
-      <input type="text"  id="model" name="model" value={formData.model} onChange={handleChange}/>
-      <br></br><br></br>
-      </div>
+    <div id={`step${currentStep}`}>
+      {currentStep == 1 && (
+        <>
+          <h1>Customer Details</h1>
+          <label htmlFor="first_name">First Name:</label>
+          <input
+            type="text"
+            id="first_name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="last_name">Last Name:</label>
+          <input
+            type="text"
+            id="last_name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+          <br></br>
+          <br></br>
+        </>
       )}
-      {index==3 && (
-        <div id='step3'>
-        <label htmlFor="card-info">Credit Card Number:</label>
-        <input id="card-info" type="tel" pattern="[0-9\s]{13,19}" minLength="0"
-          maxLength="12" name='card_info' inputMode='numeric'
-         placeholder="xxxx xxxx xxxx xxxx" required value={formData.card_info} onChange={handleChange}/>
-         <br></br><br></br>
-         <label htmlFor='expiry-date'>Expiry Date:</label>
-         <input id='expiry-date' type="date" name="expiry_date" value={formData.expiry_date} onChange={handleChange}></input>
-         <br></br><br></br>
-         </div>
+      {currentStep == 2 && (
+        <>
+          <h1>Car Details</h1>
+          <label htmlFor="car_price">Make:</label>
+          <input
+            type="text"
+            id="car_price"
+            name="make"
+            value={formData.make}
+            onChange={handleChange}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="model">Model:</label>
+          <input
+            type="text"
+            id="model"
+            name="model"
+            value={formData.model}
+            onChange={handleChange}
+          />
+          <br></br>
+          <br></br>
+        </>
       )}
-      {index>1 && (
-        <button onClick={()=>handlePrev(index)}>Previous</button>
+      {currentStep == 3 && (
+        <>
+          <h1>Payment Details</h1>
+          <label htmlFor="card-info">Credit Card Number:</label>
+          <input
+            id="card-info"
+            type="tel"
+            pattern="[0-9\s]{13,19}"
+            minLength="0"
+            maxLength="12"
+            name="card_info"
+            inputMode="numeric"
+            placeholder="xxxx xxxx xxxx xxxx"
+            required
+            value={formData.card_info}
+            onChange={handleChange}
+          />
+          <br></br>
+          <br></br>
+          <label htmlFor="expiry-date">Expiry Date:</label>
+          <input
+            id="expiry-date"
+            type="date"
+            name="expiry_date"
+            value={formData.expiry_date}
+            onChange={handleChange}
+          ></input>
+          <br></br>
+          <br></br>
+        </>
       )}
-      {index<3 &&(
-          <button onClick={()=>handleNext(index)}>Next</button>
+      {currentStep > 1 && (
+        <button onClick={() => handlePrev(currentStep)}>Previous</button>
       )}
-      {index==3 && (
-       <button onClick={handleSubmit}>Submit</button>
+      {currentStep < 3 && (
+        <button onClick={() => handleNext(currentStep)}>Next</button>
       )}
-  
+      {currentStep == 3 && <button onClick={handleSubmit}>Submit</button>}
     </div>
-  )
-}
+  );
+};
 
-export default Step
+export default Step;
